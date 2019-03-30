70c237cf5c2aa25276e0de613776ebdde9409c87
Giorgos Charitakis <chgiorgos13@gmail.com>
2019-03-30 20:10:36 +0200

    Added checkboxes in todo list items

--- index.js
+++ index.js
@@ -2,21 +2,16 @@ window.onload = function() {
     new Vue({
         el: "#todo-list-container",
         data: {
-            items: [
-                {
-                    id: 0,
-                    label: "some"
-                }
-            ],
+            items: [itemWithValues(0, "some", true)],
             nextID: 1
         },
         methods: {
             add: function() {
-                // this.items.push("some" + this.nextID++);
-                this.items.push({
-                    id: this.nextID++,
-                    label: "some"
-                });
+                this.items.push(itemWithValues(this.nextID++, "some", false));
+            },
+            setDone: function(item) {
+                var index = this.items.indexOf(item);
+                this.items[index].done = !this.items[index].done;
             },
             remove: function(item) {
                 var index = this.items.indexOf(item);
@@ -26,3 +21,11 @@ window.onload = function() {
         }
     });
 };
+
+function itemWithValues(id, label, done) {
+    return {
+        id: id,
+        label: label,
+        done: done
+    };
+}
